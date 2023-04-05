fdea614b1271b2c651afab37a186c4e4b798bde4
SM4142 <126769250+SM4142@users.noreply.github.com>
2023-04-05 16:15:35 +0300

    Add files via upload

---
+++ Doktor.kt
@@ -0,0 +1,7 @@
+fun main(){
+
+}
+class Doktor(val isim: String, val soyAd: String, val brans: String, val departman: String, val birthDate: String) {
+    fun yaz(): String {
+        return "Doktor $isim $soyAd, $departman $brans Polikliniğinde  departmanında çalışmaktadır." }
+}
---
+++ odev.kt
@@ -0,0 +1,4 @@
+fun main(){
+    val doctor = Doktor("Mehmet", "Bandırma", "Çocuk", "Poliklinik", "10.12.1975")
+    println(doctor.yaz())
+}
